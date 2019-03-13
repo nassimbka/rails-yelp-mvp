9173db93c6a53076d99efe2afd580c7f5e4775ea
Nassim Boukhalfa <nassim106@hotmail.com>
2019-03-13 14:12:21 +0100

    controllers OK
    Signed-off-by: Nassim Boukhalfa <nassim106@hotmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # get 'restaurants/index'
   # get 'restaurants/show'
   # get 'restaurants/new'
   # get 'restaurants/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews
 end
