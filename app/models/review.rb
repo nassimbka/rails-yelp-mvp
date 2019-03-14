bbf305c980486e52306b82dd137cd0112ea88ed2
Nassim Boukhalfa <nassim106@hotmail.com>
2019-03-14 11:39:09 +0100

    final push
    Signed-off-by: Nassim Boukhalfa <nassim106@hotmail.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,6 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: [0, 1, 2, 3, 4, 5]
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
   # validates :rating, numericality: { only_integer: true }, inclusion: [0, 1, 2, 3, 4, 5]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
+  # get 'reviews/new'
   # get 'restaurants/index'
   # get 'restaurants/show'
   # get 'restaurants/new'
